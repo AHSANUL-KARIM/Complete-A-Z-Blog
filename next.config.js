e9ad313c2663dfc73438a4ebc2f122b3414bbe59
AHSANUL-KARIM <ahsan5304@gmail.com>
2021-12-11 14:37:56 +0600

    prepared for deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  env: {
+    mongodb_username: 'AHSANUL',
+    mongodb_password: 'AHsanul12',
+    mongodb_clustername: 'cluster0',
+    mongodb_database: 'my-site'
+   }
 }
